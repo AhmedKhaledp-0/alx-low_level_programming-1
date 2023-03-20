b5965ecf65e1cad96969bf529e054045049387c4
sundayefeterry <info.techbro.set@gmail.com>
2023-03-20 08:19:04 -0700

    Fixedgit add .

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-<<<<<<< HEAD
 gcc -Wall -pedantic -Werror -Wextra -C *.C
 ar -rc liball.a *.o
 ranlib liball.a
-=======
 gcc -c *.c
 ar -rcs liball.a *.o
->>>>>>> 4d4f96bc98053bf24631fc8eeffcfc134523a663
