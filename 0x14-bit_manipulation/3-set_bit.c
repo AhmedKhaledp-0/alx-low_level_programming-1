5f211550422a4802796072a3496138b070c989aa
SundayEfeTerry <info.techbro.set@gmail.com>
2023-04-07 04:33:35 +0100

    0x14-bit_manipulation
    0x14-bit_manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * set_bit - sets a bit at a given index to 1
- * @n: pointer to the number to change
- * @index: index of the bit to set to 1
+ * set_bit - bit at a given index
+ * @n: pointer to the number
+ * @index: index of bit to set
  *
- * Return: 1 for success, -1 for failure
+ * Return: 1 success, -1 failure
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * clear_bit - Clears a bit at a given index in an unsigned long integer.
+ * @n: A pointer to the unsigned long integer.
+ * @index: The index of the bit to clear (starting from 0).
+ *
+ * Return: 1 if successful.
+ *          otherwise -1.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+    if (index >= sizeof(unsigned long int) * 8)
+        return (-1);
+
+    unsigned long int mask = 1UL << index;
+    *n &= ~mask;
+
+    return (1);
+}
