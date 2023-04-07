9825c27a987c4e96f5dab51436e8f5cb5f55887b
sundayefeterry <info.techbro.set@gmail.com>
2023-04-06 20:19:13 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,18 +9,18 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    int holder;
-    int counter = 0;
-    unsigned long int new_num;
-    unsigned long int ex_num = n ^ m;
-    int num = sizeof(unsigned long int) * 8 - 1;
+	int holder;
+	int counter = 0;
+	unsigned long int new_num;
+	unsigned long int ex_num = n ^ m;
+	int num = sizeof(unsigned long int) * 8 - 1;
 
-    for (holder = num; holder >= 0; holder--)
-    {
-        new_num = ex_num >> holder;
-        if (new_num & 1)
-            counter++;
-    }
+	for (holder = num; holder >= 0; holder--)
+	{
+		new_num = ex_num >> holder;
+		if (new_num & 1)
+			counter++;
+	}
 
-    return counter;
+	return (counter);
 }
