3a6058a592a5c7c63d6268067d5623fcafae033c
sundayefeterry <info.techbro.set@gmail.com>
2023-04-08 02:12:31 -0700

    0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,6 +5,7 @@
 #define BUFFER_SIZE 1024
 
 char *allocate_buffer(const char *filename);
+
 void close_file(int fd);
 
 /**
@@ -15,14 +16,15 @@ void close_file(int fd);
  */
 char *allocate_buffer(const char *filename)
 {
-    char *buffer = malloc(sizeof(char) * BUFFER_SIZE);
-    if (buffer == NULL)
-    {
-        dprintf(STDERR_FILENO,
-            "Error: Can't allocate memory for buffer\n");
-        exit(EXIT_FAILURE);
-    }
-    return (buffer);
+	char *buffer = malloc(sizeof(char) * BUFFER_SIZE);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+				"Error: Can't allocate memory for buffer\n");
+		exit(EXIT_FAILURE);
+	}
+	return (buffer);
 }
 
 /**
@@ -31,12 +33,12 @@ char *allocate_buffer(const char *filename)
  */
 void close_file(int fd)
 {
-    if (close(fd) == -1)
-    {
-        dprintf(STDERR_FILENO,
-            "Error: Can't close file descriptor %d\n", fd);
-        exit(EXIT_FAILURE);
-    }
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO,
+				"Error: Can't close file descriptor %d\n", fd);
+		exit(EXIT_FAILURE);
+	}
 }
 
 /**
@@ -48,56 +50,55 @@ void close_file(int fd)
  */
 int main(int argc, char *argv[])
 {
-    int input_fd, output_fd, bytes_read, bytes_written;
-    char *buffer;
+	int input_fd, output_fd, bytes_read, bytes_written;
+	char *buffer;
 
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: %s source_file dest_file\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s source_file dest_file\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
-    buffer = allocate_buffer(argv[1]);
-    input_fd = open(argv[1], O_RDONLY);
-    if (input_fd == -1)
-    {
-        dprintf(STDERR_FILENO,
-            "Error: Can't open file %s for reading\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+	buffer = allocate_buffer(argv[1]);
+	input_fd = open(argv[1], O_RDONLY);
+	if (input_fd == -1)
+	{
+		dprintf(STDERR_FILENO,
+				"Error: Can't open file %s for reading\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
-    output_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-    if (output_fd == -1)
-    {
-        dprintf(STDERR_FILENO,
-            "Error: Can't open file %s for writing\n", argv[2]);
-        exit(EXIT_FAILURE);
-    }
+	output_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (output_fd == -1)
+	{
+		dprintf(STDERR_FILENO,
+				"Error: Can't open file %s for writing\n", argv[2]);
+		exit(EXIT_FAILURE);
+	}
 
-    do
-    {
-        bytes_read = read(input_fd, buffer, BUFFER_SIZE);
-        if (bytes_read == -1)
-        {
-            dprintf(STDERR_FILENO,
-                "Error: Can't read from file %s\n", argv[1]);
-            exit(EXIT_FAILURE);
-        }
+	do {
+		bytes_read = read(input_fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Error: Can't read from file %s\n", argv[1]);
+			exit(EXIT_FAILURE);
+		}
 
-        bytes_written = write(output_fd, buffer, bytes_read);
-        if (bytes_written == -1)
-        {
-            dprintf(STDERR_FILENO,
-                "Error: Can't write to file %s\n", argv[2]);
-            exit(EXIT_FAILURE);
-        }
-    } while (bytes_read > 0);
+		bytes_written = write(output_fd, buffer, bytes_read);
+		if (bytes_written == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Error: Can't write to file %s\n", argv[2]);
+			exit(EXIT_FAILURE);
+		}
+	} while (bytes_read > 0);
 
-    close_file(input_fd);
+	close_file(input_fd);
 
-    close_file(output_fd);
-	
-    free(buffer);
+	close_file(output_fd);
 
-    return (EXIT_SUCCESS);
+	free(buffer);
+
+	return (EXIT_SUCCESS);
 }
