a7683ca8235b8eccff226decfc492b0536a9c97b
SundayEfeTerry <info.techbro.set@gmail.com>
2023-04-08 10:07:27 +0100

    0x15-file_io
    0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,91 +1,103 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 1024
+
+char *allocate_buffer(const char *filename);
+void close_file(int fd);
 
 /**
- * print_error - file error and close.
- * @file_handle: file handle.
+ * allocate_buffer - Allocates a buffer of BUFFER_SIZE bytes.
+ * @filename: The name of the file to be read.
  *
- * Return: Null.
+ * Return: A pointer to the allocated buffer.
  */
-
-void print_error(int file_handle)
+char *allocate_buffer(const char *filename)
 {
-	dprintf(STDERR_FILENO, "Error!! files closed!! %d\n", file_handle);
-
-	exit(100);
+    char *buffer = malloc(sizeof(char) * BUFFER_SIZE);
+    if (buffer == NULL)
+    {
+        dprintf(STDERR_FILENO,
+            "Error: Can't allocate memory for buffer\n");
+        exit(EXIT_FAILURE);
+    }
+    return (buffer);
 }
 
 /**
- * main - Duplicate content of a file >> another file.
- * @argc: Arguments.
- * @argv: Array.
+ * close_file - Closes a file descriptor.
+ * @fd: The file descriptor to be closed.
+ */
+void close_file(int fd)
+{
+    if (close(fd) == -1)
+    {
+        dprintf(STDERR_FILENO,
+            "Error: Can't close file descriptor %d\n", fd);
+        exit(EXIT_FAILURE);
+    }
+}
+
+/**
+ * main - Copies the contents of one file to another file.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of strings containing the program arguments.
  *
- * Return: Success.
+ * Return: 0 on success, or a non-zero value on failure.
  */
 int main(int argc, char *argv[])
 {
-	int file_orginal_location, file_new_location, new_w_c;
+    int input_fd, output_fd, bytes_read, bytes_written;
+    char *buffer;
 
-	char buffer[1024];
+    if (argc != 3)
+    {
+        dprintf(STDERR_FILENO, "Usage: %s source_file dest_file\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "COPY: file to new location\n");
+    buffer = allocate_buffer(argv[1]);
+    input_fd = open(argv[1], O_RDONLY);
+    if (input_fd == -1)
+    {
+        dprintf(STDERR_FILENO,
+            "Error: Can't open file %s for reading\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
 
-		exit(97);
-	}
+    output_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+    if (output_fd == -1)
+    {
+        dprintf(STDERR_FILENO,
+            "Error: Can't open file %s for writing\n", argv[2]);
+        exit(EXIT_FAILURE);
+    }
 
-	file_orginal_location = open(argv[1], O_RDONLY);
+    do
+    {
+        bytes_read = read(input_fd, buffer, BUFFER_SIZE);
+        if (bytes_read == -1)
+        {
+            dprintf(STDERR_FILENO,
+                "Error: Can't read from file %s\n", argv[1]);
+            exit(EXIT_FAILURE);
+        }
 
-	if (argv[1] == NULL || file_orginal_location < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: No such file in path %s\n", argv[1]);
+        bytes_written = write(output_fd, buffer, bytes_read);
+        if (bytes_written == -1)
+        {
+            dprintf(STDERR_FILENO,
+                "Error: Can't write to file %s\n", argv[2]);
+            exit(EXIT_FAILURE);
+        }
+    } while (bytes_read > 0);
 
-		exit(98); }
+    close_file(input_fd);
 
-	file_new_location = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+    close_file(output_fd);
+	
+    free(buffer);
 
-	if (file_new_location < 0)
-	{
-		dprintf(STDERR_FILENO, "Can't write file to %s\n", argv[2]);
-
-		close(file_orginal_location);
-
-		exit(99); }
-
-	while ((new_w_c = read(file_orginal_location, buffer, 1024)) > 0)
-	{
-		if (new_w_c != write(file_new_location, buffer, new_w_c))
-		{
-			dprintf(STDERR_FILENO, "Stop: Can't write to %s\n", argv[2]);
-
-			close(file_orginal_location);
-
-			close(file_new_location);
-
-			exit(99);
-		}
-	}
-
-	int i;
-	i = 0;
-
-	if (new_w_c < i)
-	{
-		dprintf(STDERR_FILENO, "Error! Can't read from file: %s\n", argv[1]);
-
-		exit(98);
-	}
-
-	if (close(file_orginal_location) < 0)
-	{
-		print_error(file_orginal_location);
-	}
-
-	if (close(file_new_location) < 0)
-	{
-		print_error(file_new_location);
-	}
-
-	return (0);
-
+    return (EXIT_SUCCESS);
 }
