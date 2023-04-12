bbd8460dd2784d506da8753bab2cd21b5e048069
SundayEfeTerry <info.techbro.set@gmail.com>
2023-04-12 09:31:21 +0100

    0x15-file_io
    0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,70 +1,110 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_buffer(char *file_md);
+
+void close_file(int file_dir);
 
 /**
- * main - copy content of file a to file b.
- * @argc: number of arguments passed.
- * @argv: array of arguments passed.
+ * create_buffer - This function fress 1024 bytes for a buffer.
  *
- * Return: 0
+ * @file_md: buffer destination storing chars.
+ *
+ *
+ * Return: pointer destination new allocated buffer.
  */
+
+char *create_buffer(char *file_md)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (!buffer)
+	{
+		dprintf(STDERR_FILENO, "Error!! Write Error destination %s\n", file_md);
+		exit(99);
+	}
+
+	return (buffer);
+}
+
+/**
+ * close_file - will close file_md.
+ *
+ * @file_dir: file destination be closed.
+ */
+
+void close_file(int file_dir)
+{
+	int cls;
+
+	cls = close(file_dir);
+
+	if (cls == -1)
+	{
+		dprintf(STDERR_FILENO, "Error!! close file_dir error!! %d\n", file_dir);
+		exit(100);
+	}
+}
+
+/**
+ * main - Copy contents of file_md destination another file_md.
+ * @argc: number of arguments gotten.
+ * @argv: array of pointers destination arguments.
+ *
+ * Return: 0 on success.
+ */
+
 int main(int argc, char *argv[])
 {
-	int file_read;
-	int file_write;
-	int rd;
-	int a;
-	int b;
+	int src;
+	int destination;
+	int r_file;
+	int w_file;
 
-	char buff[BUFSIZ];
+	char *buffer;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Copied! \n");
+		dprintf(STDERR_FILENO, "Copy!! \n");
 		exit(97);
 	}
 
-	file_read = open(argv[1], O_RDONLY);
+	buffer = create_buffer(argv[2]);
+	src = open(argv[1], O_RDONLY);
+	r_file = read(src, buffer, 1024);
+	destination = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	if (file_read < 0)
-	{
-		dprintf(STDERR_FILENO, "Can't read file!! Error! EXIT!! %s\n", argv[1]);
-		exit(98);
-	}
+	do {
+		if (src == -1 || r_file == -1)
+		{
+			dprintf(STDERR_FILENO, "Can't read file!! Error! EXIT!! %s\n", argv[1]);
+			free(buffer);
 
-	file_write = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+			exit(98);
+		}
 
-	while ((rd = read(file_read, buff, BUFSIZ)) > 0)
-	{
-		if (file_write < 0 || write(file_write, buff, rd) != rd)
+		w_file = write(destination, buffer, r_file);
+		if (destination == -1 || w_file == -1)
 		{
 			dprintf(STDERR_FILENO, "Can't write file!! Error! EXIT!! %s\n", argv[2]);
-			close(file_read);
+			free(buffer);
+
 			exit(99);
 		}
-	}
-	if (rd < 0)
-	{
-		dprintf(STDERR_FILENO, "Can't read file!! Error! EXIT!! %s\n", argv[1]);
-		exit(98);
-	}
 
-	a = close(file_read);
-	b = close(file_write);
+		r_file = read(src, buffer, 1024);
+		destination = open(argv[2], O_WRONLY | O_APPEND);
 
-	if (!(a > 0 && b > 0))
-	{
-		if (a < 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_read);
-		}
+	} while (r_file > 0);
 
-		if (b < 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_write);
-		}
+	free(buffer);
 
-		exit(100);
-	}
+	close_file(src);
+
+	close_file(destination);
 
 	return (0);
 }
